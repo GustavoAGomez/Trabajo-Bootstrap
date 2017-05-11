1712e8067f23daa0a9c1cfb2b1df48b917ed3526
gustavo <gustavo@pixelandpixel.com>
2017-05-11 13:52:24 +0200

    mas cambios

--- bootstrap work/js/script.js
+++ bootstrap work/js/script.js
@@ -4,13 +4,13 @@ $(function(){
 
 
 	//Animacion TweenMax para footer
-	var tweenbox = new TweenMax.staggerTo('.form-group', 1, {bottom: 0,rotationY: 360, ease: Back.easeOut}); 
+	var tweenbox = new TweenMax.staggerTo('.form-horizontal', 1, {bottom: 0,rotationY: 360, ease: Back.easeOut}); 
 
 
 	//Incluir TweenMax y sus propiedades al scroll magic
 	var scene3 = new ScrollMagic.Scene({ 				
 										triggerElement: '#trigger',
-										offset: 315,
+										offset: 290,
 										triggerHook: "onEnter"
 										})
 										.setTween(tweenbox)
