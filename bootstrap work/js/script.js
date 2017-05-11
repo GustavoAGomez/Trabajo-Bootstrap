b8cb6789dedbc8666688b34e849e5daa677501f0
gustavo <gustavo@pixelandpixel.com>
2017-05-11 15:05:40 +0200

    modif footer

--- bootstrap work/js/script.js
+++ bootstrap work/js/script.js
@@ -3,17 +3,29 @@ $(function(){
 	var controller = new ScrollMagic.Controller();  	//Controlador de scrollmagic
 
 
-	//Animacion TweenMax para footer
-	var tweenbox = new TweenMax.staggerTo('.form-horizontal', 1, {bottom: 0,rotationY: 360, ease: Back.easeOut}); 
+	//Animacion TweenMax para formulario
+	var tweenform = new TweenMax.staggerTo('.form-horizontal', 1, {bottom: 0, ease: Bounce.easeOut}); 
+
+	//Animacion TweenMax para informacion-footer
+	var tweeninfo = new TweenMax.staggerTo('.go-plus', 1, {left: 0, ease: Bounce.easeOut}); 
 
 
 	//Incluir TweenMax y sus propiedades al scroll magic
-	var scene3 = new ScrollMagic.Scene({ 				
+	var SceneForm = new ScrollMagic.Scene({ 				
 										triggerElement: '#trigger',
 										offset: 290,
 										triggerHook: "onEnter"
 										})
-										.setTween(tweenbox)
+										.setTween(tweenform)
+										.addIndicators()
+										.addTo(controller);
+
+	var SceneGoPlus = new ScrollMagic.Scene({ 				
+										triggerElement: '#trigger',
+										offset: 290,
+										triggerHook: "onEnter"
+										})
+										.setTween(tweeninfo)
 										.addIndicators()
 										.addTo(controller);
 
